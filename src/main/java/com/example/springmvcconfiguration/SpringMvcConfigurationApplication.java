591c211523457e93999ab7e1b7091d98e36be7d2
ZAIvanov18 <ZAIvanov18@codingburgas.bg>
2022-11-21 12:26:52 +0200

    MVC Config

--- src/main/java/com/example/springmvcconfiguration/SpringMvcConfigurationApplication.java
+++ src/main/java/com/example/springmvcconfiguration/SpringMvcConfigurationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringMvcConfigurationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMvcConfigurationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcConfigurationApplication.class, args);
+    }
 
 }
